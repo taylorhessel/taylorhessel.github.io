3ec8c669fa32f937fa857e0679510e4e9e5d0fcf
Taylor Hessel <taylorhessel@gmail.com>
2017-04-27 16:09:35 -0600

    Relative path webpack issue

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'assets',
+    assetsSubDirectory: '/assets',
     assetsPublicPath: '/dist',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as

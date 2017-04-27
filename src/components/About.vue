743a23e99dd099d3ec0efc2455ede0497e52d3dd
Taylor Hessel <taylorhessel@gmail.com>
2017-04-27 16:14:08 -0600

    I don't have time for this, webpack

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <div class="view about ink-flex">
     <div class="column-group push-center">
       <div class="tiny-100 small-100 medium-100 large-40 xlarge-40 bio">
-        <img src="../assets/images/taylor.jpg" alt="hey, that's me">
+        <img src="/src/assets/images/taylor.jpg" alt="hey, that's me">
         <p>My name is <a href="https://www.linkedin.com/in/taylor-hessel-44615848/" target="blank">Taylor Hessel</a>, and I'm a full-stack developer living in Denver, CO. I like gaming and cooking, and I love my cats. <a href="https://github.com/taylorhessel" target="blank">Coding</a> is pretty fun, too.</p>
       </div>
       <div class="tiny-100 small-100 medium-100 large-60 xlarge-60">

26f61585297ec5ae2b32744d31321a94ccfa37a1
Taylor Hessel <taylorhessel@gmail.com>
2017-05-01 16:03:47 -0600

    Webpack still being difficult

--- config/index.js
+++ config/index.js
@@ -5,9 +5,9 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'assets',
-    assetsPublicPath: '/dist',
+    assetsRoot: path.resolve(__dirname, '../public'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
